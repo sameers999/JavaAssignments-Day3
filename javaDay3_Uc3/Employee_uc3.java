9610cc0c21142a72fdd837d0849183a4e8c3d9e6
sameers999 <syedmahammadsameer999>
2022-03-24 15:58:11 +0530

    EmployeeWage PartTime

---
+++ javaDay3_Uc3/Employee_uc3.java
@@ -0,0 +1,22 @@
+package javaDay3_Uc3;
+
+public class Employee_uc3 {
+	public static void main(String[] args) {
+		int IS_PART_TIME =1;
+		int IS_FULL_TIME =2;
+		int EMP_RATE_PER_HOUR = 20;
+		//variables
+		int empHrs = 0;
+		int empWage = 0;
+		//computation
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		if (empCheck == IS_PART_TIME)
+			empHrs = 4;
+		else if (empCheck == IS_FULL_TIME)
+			empHrs = 8;
+		else
+			empHrs = 0;
+		empWage =empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
+	}
+}
